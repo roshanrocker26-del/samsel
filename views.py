3ac19bb10aaf667eb97774949e154070cddf9ffc
roshanrocker26-del <roshanrocker26@gmail.com>
2026-01-06 12:45:50 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def teacher_login(request):
+    return render(request, 'teacher_login.html')
+
+def student_login(request):
+    return render(request, 'student_login.html')
---
+++ wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for samsel_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'samsel_project.settings')
+
+application = get_wsgi_application()
